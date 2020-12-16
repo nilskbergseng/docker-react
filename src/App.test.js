42afd28de6dc8d14d4de8a49f67540a91c663e61
Nils-Kristian Bergseng <nkb@capraconsulting.no>
2020-12-16 08:03:19 +0100

    fix unittest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react linkssaaaaa", () => {
-  render(<Apps />);
+test("renders learn react links", () => {
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
